d83aadc4ce1628fb5a1eefeb2293884aa9cf3ca1
Nithin Reddy <nithinpoolareddy@gmail.com>
2025-10-08 13:15:14 -0400

    feat: Add new test component

---
+++ components/providers/blank.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyNewComponent = () => {
+  return (
+    <div>
+      <h1>This is my new test component!</h1>
+      <p>I am learning the Git push workflow.</p>
+    </div>
+  );
+};
+
+export default MyNewComponent;
